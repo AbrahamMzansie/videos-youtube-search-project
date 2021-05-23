fcd03042199bc0880566f7179db17714a1e401ba
unknown <abrahaam.nkomo@gmail.com>
2021-05-23 14:49:02 +0200

    test

---
+++ src/components/VideoRecorder.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const VideoRecorder = (props)=>{
+
+    return (
+        <>
+        <div>
+            Welcome To tutorial
+        </div>
+        </>
+    )
+}
+export default VideoRecorder;
